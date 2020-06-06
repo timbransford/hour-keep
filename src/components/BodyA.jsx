f59dcc3168b365b310d4868fd6b549332acfe4fa
Tim Bransford <bransfordware@gmail.com>
2020-06-06 18:41:08 -0400

    Change fonts

--- src/components/BodyA.jsx
+++ src/components/BodyA.jsx
@@ -2,35 +2,26 @@ import React from "react";
 
 function BodyA() {
   return (
-        <div class="container">
-          <div class="row">
-            <div class="col-lg-1 col-md-0"></div>
-            <div class="col-lg-10 col-md-12">
-              <img class="col-lg-10 col-md-8 app-image" alt="ship on dock" 
-                src="https://res.cloudinary.com/hblxshr0j/image/upload/v1591473551/hour-keep/Ship_jvkrjc.jpg" />
-            </div>
-            <div class="col-lg-1 col-md-0"></div>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-1 col-md-0"></div>
+          <div class="col-lg-10 col-md-12">
+            <img class="col-lg-10 col-md-8 app-image" alt="ship on dock" 
+              src="https://res.cloudinary.com/hblxshr0j/image/upload/v1591473551/hour-keep/Ship_jvkrjc.jpg" />
           </div>
-          <div class="row">
-            <div class="col-lg-1 col-md-0"></div>
-            <div class="col-lg-10 col-md-12 app-body">
-              <p><span class="app-name-text">Got Your Hours?</span> is a new app built specifically with Longshoreman and Dockworkers in mind.
-                A simple interface to enter your hours and keep track
-                of your paycheck and benefit hours quickly securely and easily. 
-              </p>
-            </div>
-            <div class="col-lg-1 col-md-0"></div>
+          <div class="col-lg-1 col-md-0"></div>
+        </div>
+        <div class="row">
+          <div class="col-lg-1 col-md-0"></div>
+          <div class="col-lg-10 col-md-12 app-body">
+            <p><span class="app-name-text">Got Your Hours?</span> is a new app built specifically with Longshoreman and Dockworkers in mind.
+              You will be amazed at how simple the app is to use.  Track your
+              paycheck and benefit hours quickly securely and easily. 
+            </p>
+            <p>Be one of the first to find how <span class="app-name-text">Got Your Hours?</span> will simplify your life.</p>
+              
           </div>
-          <div class="row">
-            <div class="col-lg-1 col-md-0"></div>
-            <div class="col-lg-10 col-md-12 app-body">
-              <p>
-               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ultrices neque ornare aenean euismod elementum nisi quis. Imperdiet massa tincidunt nunc pulvinar sapien et ligula ullamcorper. Condimentum id venenatis a condimentum vitae sapien. Fermentum dui faucibus in ornare quam. Metus vulputate eu scelerisque felis imperdiet proin fermentum. Varius quam quisque id diam vel quam. Interdum consectetur libero id faucibus nisl tincidunt eget nullam non. Interdum velit euismod in pellentesque. Nec nam aliquam sem et. Eget mi proin sed libero. Volutpat blandit aliquam etiam erat velit. Mi quis hendrerit dolor magna eget est. Vel risus commodo viverra maecenas accumsan lacus vel facilisis. Bibendum enim facilisis gravida neque convallis a cras semper.
-
-              </p>
-            </div>
-            <div class="col-lg-1 col-md-0"></div>
-          </div>
+        </div>
       </div>
       );
 }
