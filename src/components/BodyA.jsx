fac5b3a4aea88661f26cb8838ce6ad56223fa4fc
Tim Bransford <bransfordware@gmail.com>
2020-06-06 20:37:24 -0400

    Made image responsive

--- src/components/BodyA.jsx
+++ src/components/BodyA.jsx
@@ -4,22 +4,24 @@ function BodyA() {
   return (
       <div class="container">
         <div class="row">
-          <div class="col-lg-1 col-md-0"></div>
-          <div class="col-lg-10 col-md-12">
-            <img class="col-lg-10 col-md-8 app-image" alt="ship on dock" 
+          <div class="col-lg-2 col-md-1"></div>
+          <div class="col-lg-8 col-md-10 top-image">
+            <img class="app-image img-responsive" alt="ship on dock" 
               src="https://res.cloudinary.com/hblxshr0j/image/upload/v1591473551/hour-keep/Ship_jvkrjc.jpg" />
-          </div>
-          <div class="col-lg-1 col-md-0"></div>
-        </div>
-        <div class="row">
-          <div class="col-lg-1 col-md-0"></div>
-          <div class="col-lg-10 col-md-12 app-body">
-            <p><span class="app-name-text">Got Your Hours?</span> is a new app built specifically with Longshoreman and Dockworkers in mind.
-              You will be amazed at how simple the app is to use.  Track your
-              paycheck and benefit hours quickly securely and easily. 
-            </p>
-            <p>Be one of the first to find how <span class="app-name-text">Got Your Hours?</span> will simplify your life.</p>
+            <div class="carousel-caption">
+              <p><span class="app-name-text">Got Your Hours?</span><br/>
+              is a new app built with<br/><span class="underline">dockworkers</span> in mind.
+              </p>
               
+            </div>
+          </div>
+        </div>
+        <div class="row body-text">
+          <div class="col-lg-2 col-md-1"></div>
+          <div class="col-lg-8 col-md-10">
+              <p>You will be amazed at how simple it is to use.  Track your
+                paycheck and benefit hours quickly and securely. 
+               Be one of the first to find how <span class="app-name-text">Got Your Hours?</span> life.</p>
           </div>
         </div>
       </div>
